c3fc4efe1c5f637c8fdb94a3624a70b261d6a11d
debc <debracupitt@gmail.com>
2018-08-19 17:25:37 +0700

    Week 5 work

---
+++ Week5JavaScript/soc_wk5_cert_debra_cupitt.js
@@ -0,0 +1,8 @@
+// =================== WEEK 1 HOMEWORK ====================
+
+// ===================  DAY 1 ====================
+
+// Hours in a year. How many hours are in a year?
+// Minutes in a decade. How many minutes are in a decade?
+// Your age in seconds. How many seconds old are you? (I'm not going to check your answer, so be as accurate—or not—as you want.)
+// Cristina Tarantino: 32 yesterday! How many milliseconds old is she hahaha? Calculate @Cristina Tarantino's age in milliseconds.
